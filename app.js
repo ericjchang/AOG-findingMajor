fbd22b0c4021abfc27deb8f72cb35a662c369b7f
ericjchang <eric.jaspchang@gmail.com>
2020-08-02 02:54:35 +0700

    initialize server using express, install required depedencies, initialize sequelize

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+const routes = require('./routes');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));

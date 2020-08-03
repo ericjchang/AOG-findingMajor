9c42435dfe526d2a2bfc8216b7fb5ccd9010481f
ericjchang <eric.jaspchang@gmail.com>
2020-08-04 03:05:31 +0700

    add environment for redis

--- helpers/queueSendEmail.js
+++ helpers/queueSendEmail.js
@@ -1,6 +1,7 @@
 const { sendEmailRegistration } = require('./nodemailer');
 const Bull = require('bull');
-const queueSendEmail = new Bull('send-email');
+let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+const queueSendEmail = new Bull('send-email', REDIS_URL);
 
 function registrationEmail(email) {
   queueSendEmail.add({ email });
